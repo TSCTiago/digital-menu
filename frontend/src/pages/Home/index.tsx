bf9922e09f5f3395180ef99b42635e4eccf88c53
TSCTiago <tiagodasilvacar@gmail.com>
2024-10-31 23:26:50 -0300

    chore: update

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -45,6 +45,12 @@ const restaurants = [
     photo:
       "https://marketplace.canva.com/EAFpeiTrl4c/1/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-9Gfim1S8fHg.jpg",
   },
+  {
+    id: 5,
+    name: "Pasta & Co.",
+    photo:
+      "https://marketplace.canva.com/EAFpeiTrl4c/1/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-9Gfim1S8fHg.jpg",
+  }
 ];
 
 export function HomePage() {
